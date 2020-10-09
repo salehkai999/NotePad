4170237dc9a04b7f77c2380a7420793b3e6ab80f
saleh <saleh.mk96@gmail.com>
2020-10-09 13:37:31 -0500

    Added recycler view, notes, adapter and viewholder. Row layout, mockup data working

--- app/src/main/java/com/saleh/notepad/MainActivity.java
+++ app/src/main/java/com/saleh/notepad/MainActivity.java
@@ -1,14 +1,31 @@
 package com.saleh.notepad;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    private final List<Note> noteList = new ArrayList<>();
+    private RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recycler);
+        NoteAdapter noteAdapter = new NoteAdapter(noteList,this);
+        recyclerView.setAdapter(noteAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        for(int i=0;i<10;i++) {
+            noteList.add(new Note(i+"","SSSSSAAAA",new Date()));
+        }
+
     }
 }
